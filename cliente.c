c1f6b5b4fbef36e35484086e30dd456946547992
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2022-05-04 18:23:53 -0600

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -1,121 +1,157 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <string>
 #include <stdbool.h>
+#include <iostream>
+#include "json.hpp"
+#include <iomanip>
+#include<string>
+#include<time.h>
 
-#define LENGTH 2048
 
-// Global variables
-volatile sig_atomic_t flag = 0;
-int sockfd = 0;
-char usuario[32];
-char status[32] = "Activo";
+using namespace std;
+using json = nlohmann::json;
+char *usuario;
 
-void str_overwrite_stdout() {
-  printf("%s", "> ");
-  fflush(stdout);
-}
+void *recvsocket(void *arg) 
+{
+    int st = *(int *)arg;
 
-void str_trim_lf (char* arr, int length) {
-  int i;
-  for (i = 0; i < length; i++) { // trim \n
-    if (arr[i] == '\n') {
-      arr[i] = '\0';
-      break;
+        while(1){
+        json request;
+        json response;
+        char buffer[1024];
+        request["request"] = "GET_CHAT";
+        request["body"] = "all";
+
+        string parsed_request = request.dump();
+
+        strcpy(buffer, parsed_request.c_str());
+        send(st, buffer, parsed_request.size()+1, 0);
+
+        int rc = recv(st, buffer, 1024, 0);
+       // if (rc <= 0) 
+         //           break;
+        response = json::parse(buffer);
+        string respons = response["response"];
+        string code = response["code"];
+        string message = response["body"][0][0];
+        string from = response["body"][0][1];
+        string delivered_at = response["body"][0][2];
+
+        if(code == "200"){
+            printf(" Mensajes Obtenidos");
+        }
     }
-  }
+    
 }
 
-void catch_ctrl_c_and_exit(int sig) {
-    flag = 1;
+
+void *sendsocket(void *arg) 
+{
+    int st = *(int *)arg;
+
+    // char client_username[100];
+    // sprintf(client_username, "NEW_CLIENT_USERNAME %s", "Amy");
+    // send(st, client_username, strlen(client_username), 0);
+
+    char s[1024];
+    
+        json request;
+        json response;
+        char buffer[1024];
+        request["request"] = "POST_CHAT";
+
+        string message;
+        cin>>message;
+        string from;
+        from += usuario;
+        time_t delivered_at = time(0);
+        string to = "all";
+        request["body"] = {message, from, ctime(&delivered_at), to};
+
+        string parsed_request = request.dump();
+
+        strcpy(buffer, parsed_request.c_str());
+        send(st, buffer, parsed_request.size()+1, 0);
+
+        int rc = recv(st, buffer, 1024, 0);
+        //if (rc <= 0) 
+          //          break;
+        response = json::parse(buffer);
+        string respons = response["response"];
+        string code = response["code"];
+
+        if(code == "200"){
+            printf(" Mensaje Enviado");
+            } 
+        
 }
 
-void send_msg_handler() {
-  char message[LENGTH] = {};
-	char buffer[LENGTH + 32] = {};
+int main(int arg, char *args[])
+{
+    string ip = "0.0.0.0";
+    int port = 6666;
+    char status[] = "ACTIVO";
+    int entrada;
 
-  while(1) {
-  	str_overwrite_stdout();
-    fgets(message, LENGTH, stdin);
-    str_trim_lf(message, LENGTH);
-
-    if (strcmp(message, "exit") == 0) {
-			break;
-    } else {
-      sprintf(buffer, "%s: %s\n", usuario, message);
-      send(sockfd, buffer, strlen(buffer), 0);
+    if (arg >= 3)
+    {
+        ip = string(args[2]);
+        port = atoi(args[3]);
+        usuario = args[1];
     }
 
-		bzero(message, LENGTH);
-    bzero(buffer, LENGTH + 32);
-  }
-  catch_ctrl_c_and_exit(2);
-}
+    int st = socket(AF_INET, SOCK_STREAM, 0); 
 
-void recv_msg_handler() {
-	char message[LENGTH] = {};
-  while (1) {
-		int receive = recv(sockfd, message, LENGTH, 0);
-    if (receive > 0) {
-      printf("%s", message);
-      str_overwrite_stdout();
-    } else if (receive == 0) {
-			break;
-    } else {
-			// -1
-		}
-		memset(message, 0, sizeof(message));
-  }
-}
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip.c_str());
 
+    if (connect(st, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
+        printf("connect failed %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+    
+    json request;
+    json response;
+    time_t hora = time(0);
 
-int main(int argc, char **argv){
-	if(argc != 2){
-		printf("Usage: %s <port>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
+    char buffer[1024];
+    request["request"] = "INIT_CONEX";
+    request["body"] = {ctime(&hora), usuario};
 
-	char *ip = "127.0.0.1";
-	int port = atoi(argv[1]);
+    string parsed_request = request.dump();
 
-	signal(SIGINT, catch_ctrl_c_and_exit);
+    strcpy(buffer, parsed_request.c_str());
+    send(st, buffer, parsed_request.size()+1, 0);
 
-	printf("Please enter your usuario: ");
-  fgets(usuario, 32, stdin);
-  str_trim_lf(usuario, strlen(usuario));
+    recv(st, buffer, 1024, 0);
+
+    response = json::parse(buffer);
+    string code = response["code"];
+    string respons = response["response"];
 
 
-	if (strlen(usuario) > 32 || strlen(usuario) < 2){
-		printf("usuario must be less than 30 and more than 2 characters.\n");
-		return EXIT_FAILURE;
-	}
-
-	struct sockaddr_in server_addr;
-
-	/* Socket settings */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-  server_addr.sin_port = htons(port);
+    if(code == "200"){
+        printf("mensaje exitoso");
+        //printf("%d", respons);
 
 
-  // Connect to Server
-  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (err == -1) {
-		printf("ERROR: connect\n");
-		return EXIT_FAILURE;
-	}
+    } 
 
-	// Send usuario
-	send(sockfd, usuario, 32, 0);
-    int entrada, entryStatus;
+    pthread_t thrd1, thrd2, thrd3;
     bool bandera = true;
 
     while(bandera)
@@ -142,78 +178,181 @@ int main(int argc, char **argv){
     printf( "---------------------------------------------------\n");
     printf( "\n");
 
-    switch (entrada)
-    {
+ 
     //Mensaje directo
-    case 1:
-    	printf("=== WELCOME TO THE CHATROOM ===\n");
+    if(entrada == 1){
+    	printf("=== BIENVENIDO AL CHAT GRUPAL ===\n");
+        pthread_create(&thrd2, NULL, sendsocket, &st);
+        pthread_create(&thrd1, NULL, recvsocket, &st);
+        pthread_join(thrd1, NULL);
+        /*while(1){
 
-        pthread_t send_msg_thread;
-        if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
-            printf("ERROR: pthread\n");
-            return EXIT_FAILURE;
-        }
+        char s[1024];
+        json request;
+        json response;
+        char buffer[1024];
+        request["request"] = "POST_CHAT";
 
-        pthread_t recv_msg_thread;
-        if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
-            printf("ERROR: pthread\n");
-            return EXIT_FAILURE;
-        }
+        string message;
+        cin>>message;
+        string from;
+        from += usuario;
+        time_t delivered_at = time(0);
+        string to = "all";
+        request["body"] = {message, from, ctime(&delivered_at), to};
 
-        while (1){
-            if(flag){
-                printf("\nBye\n");
-                break;
-        }
-        }
-        close(sockfd);
-	    return EXIT_SUCCESS;
-        break;
+        string parsed_request = request.dump();
+
+        strcpy(buffer, parsed_request.c_str());
+        send(st, buffer, parsed_request.size()+1, 0);
+
+        int rc = recv(st, buffer, 1024, 0);
+        //if (rc <= 0) 
+          //          break;
+        response = json::parse(buffer);
+        string respons = response["response"];
+        string code = response["code"];
+
+        if(code == "200"){
+            printf(" Mensaje Enviado");
+            } 
+
+        }*/
+        //pthread_create(&thrd1, NULL, recvsocket, &st);
+        //pthread_create(&thrd2, NULL, sendsocket, &st);
+        //pthread_join(thrd1, NULL);
+
+    
+    }
 
     //Broadcast
-    case 2:
+    if(entrada == 2){
+        printf("=== BIENVENIDO A LA SALA PRIVADA ===\n");
         break;
+    }
  
 
 
     //Cambio de status
-    case 3:
+    if(entrada == 3){
+        json request;
+        json response;
+        string status; 
+
+        char buffer[1024];
+        request["request"] = "PUT_STATUS";
         printf( "Cambio de estatus\n");
         printf( "Ingrese el numero de la opcion que desea:\n");
-        printf( "1. Activo\n");
-        printf( "2. Ocupado\n");
-        printf( "3. Inactivo\n");
+        printf( "0. Activo\n");
+        printf( "1. Ocupado\n");
+        printf( "2. Inactivo\n");
         printf( "\n");
         printf( "Introduzca la opcion que desea ejecutar (1-3): \n");
-        scanf("%d", &entryStatus);
         printf( "\n");
+        cin>>status;
+        request["body"] = status;
+
+        string parsed_request = request.dump();
+
+        strcpy(buffer, parsed_request.c_str());
+        send(st, buffer, parsed_request.size()+1, 0);
+
+        int rc = recv(st, buffer, 1024, 0);
+       // if (rc <= 0) 
+         //           break;
+        response = json::parse(buffer);
+        string respons = response["response"];
+        string code = response["code"];
+
+        if(code == "200"){
+            printf(" Mensajes Obtenidos");
+        }
+
+    }
 
     //Listado de usuarios
-    case 4:
+    if(entrada == 4){
+        json request;
+        json response;
+        char buffer[1024];
+        request["request"] = "GET_USER";
+        request["body"] = "all";
+
+        string parsed_request = request.dump();
+
+        strcpy(buffer, parsed_request.c_str());
+        send(st, buffer, parsed_request.size()+1, 0);
+
+        int rc = recv(st, buffer, 1024, 0);
+       // if (rc <= 0) 
+         //           break;
+        response = json::parse(buffer);
+        string respons = response["response"];
+        string code = response["code"];
+
+        if(code == "200"){
+            printf(" Usuarios Obtenidos\n");
+            for(int i=0; i<32; i++)
+                {
+                for(int j=0; j<2; j++)
+                {
+                    cout<<" "<<response["body"][i][j]<<" ";
+                }
+                cout<<"\n";
+        }
+        } 
         //obtenerInfoAllUsers(sockfd, buffer);
-        break;
+    }
 
     //Informacion de usuario
-    case 5:
-        printf( "Ingrese el usuario que desea ver: \n");
-        printf( "\n");
+    if(entrada == 5){
+        json request;
+        json response;
+        //time_t hora = time(0);
+
+        char buffer[1024];
+        request["request"] = "GET_USER";
+        string usuario;
+        cout<<"Ingrese el usuario:\n"<<endl;
+        cin>>usuario;
+        request["body"] = usuario;
+
+        string parsed_request = request.dump();
+
+        strcpy(buffer, parsed_request.c_str());
+        send(st, buffer, parsed_request.size()+1, 0);
+
+        recv(st, buffer, 1024, 0);
+
+        response = json::parse(buffer);
+        string code = response["code"];
+        string respons = response["response"];
+        string ip = response["body"][0];
+        string protocol = response["body"][1];
+
+
+        if(code == "200"){
+            cout<<"IP del usuario: "<<ip<<"\n";
+            cout<<"Estado del usuario: "<<protocol<<"\n";
+            //printf("%d", respons);
+
+
+        } 
         //obtenerInfoUsuario(IdGlobal, usuarioInf, sockfd, buffer);
-        break;
+    }
 
-    case 6:
+    if(entrada == 6){
         break;
-
+    }
     //salir
-    case 7:
+    if(entrada == 7){
         printf("SALIENDO...\n");
         bandera = false;
         break;
+    
     }
     }
 
-	close(sockfd);
-
-	return EXIT_SUCCESS;
+    close(st);
+    return EXIT_SUCCESS;
 }
-
-
